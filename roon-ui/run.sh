06affa5ef1f1e70c002652a7fd68bab808b6c16a
Flurop <flora@ratgers.nl>
2026-06-23 15:50:13 +0200

    Using correct node version now

--- roon-ui/run.sh
+++ roon-ui/run.sh
@@ -3,6 +3,4 @@ bashio::log.info "Starting Roon Web Controller on port 8099"
 
 cd /roon-web-controller
 
-echo "const util = require('util'); util.isRegExp = util.types.isRegExp; util.isDate = util.types.isDate; util.isPromise = util.types.isPromise; util.isArray = Array.isArray;" > patch.js
-
-node -r ./patch.js app.js
+node app.js
