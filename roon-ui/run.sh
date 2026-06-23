f392d313011aaa620fe16ff840856b260321c79a
Flurop <flora@ratgers.nl>
2026-06-23 15:45:22 +0200

    Fixed js warning

--- roon-ui/run.sh
+++ roon-ui/run.sh
@@ -3,6 +3,6 @@ bashio::log.info "Starting Roon Web Controller on port 8099"
 
 cd /roon-web-controller
 
-echo "const util = require('util'); util.isRegExp = util.types.isRegExp; util.isDate = util.types.isDate; util.isPromise = util.types.isPromise;" > patch.js
+echo "const util = require('util'); util.isRegExp = util.types.isRegExp; util.isDate = util.types.isDate; util.isPromise = util.types.isPromise; util.isArray = Array.isArray;" > patch.js
 
 node -r ./patch.js app.js
