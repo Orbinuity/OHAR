1808d616ae14a7b20945376cce9ea73606ba8cab
Flurop <flora@ratgers.nl>
2026-06-25 10:15:47 +0200

    Chnaged some things for the new Web UI and added verbose option

--- roon-ui/run.sh
+++ roon-ui/run.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/with-contenv bashio
-bashio::log.info "Starting Roon Web Controller on port 8099"
+bashio::log.info "Starting Roon Web UI on port 8099"
 
-cd /roon-web-controller
+cd /RoonWebUI
 
-node app.js
+if bashio::config.true 'verbose_logging'; then
+    bashio::log.info "Verbose logging is enabled (-v flag added)"
+    node app.js -p 8099 -v
+else
+    node app.js -p 8099
+fi
